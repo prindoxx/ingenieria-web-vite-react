b3b39384acc8ea0e15ea8e9c88ee8875fc02c7d7
prindoxx <pablortiz132@gmail.com>
2023-06-11 15:46:17 -0400

    validaciones login y recuperar contraseña

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,16 +3,28 @@ import './styles.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
+import { appendErrors, useForm } from "react-hook-form";
 
 
 function Login() {
+    const{register, formState: { errors }, handleSubmit} = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
     return (
         <>
         <div class="login">
             <h2>Iniciar sesión</h2>
-            <form class="login-form" id="formulario-login">
-                <input id="nombre" name="nombre" type="text" placeholder="Nombre de usuario"/>
-                <input id="contrasenya" name="contrasenya" type="password" placeholder="Contraseña"/>
+            <form class="login-form" id="formulario-login" onSubmit={handleSubmit(onSubmit)} noValidate>
+                <input id="nombre" name="nombre" type="text" placeholder="Nombre de usuario" {...register('nombre',{
+                    required: true,
+                })}/>
+                {errors.nombre?.type === 'required' && <p>El campo nombre es obligatorio</p>}
+                <input id="contrasenya" name="contrasenya" type="password" placeholder="Contraseña" {...register('contrasenya',{
+                    required: true,
+                })}/>
+                {errors.contrasenya?.type === 'required' && <p>El campo nombre es obligatorio</p>}
                 <button id="alfa" type="submit">Iniciar sesión</button>
             </form>
             <p>¿No tienes una cuenta? <Link to='/Registrarse'>Registrate</Link></p>
--- src/Recuperarcontraseña.jsx
+++ src/Recuperarcontraseña.jsx
@@ -2,18 +2,29 @@ import React from "react";
 import './styles.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import Footer from "./Footer";
+import { useForm } from "react-hook-form";
 
-function Recuperarcontraseña(){
-    return(
+function Recuperarcontraseña() {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+    return (
         <>
-        <div class="container">
-            <h1 id="register">Recuperar contraseña</h1>
-            <form id="registro">
-                <label id="register" for="email">Introduce aquí tu correo electrónico para poder recuperar tu contraseña:</label>
-                <input type="email" id="email" name="email"/>
-                <button id="registrarse" type="submit">Recuperar contraseña</button>
-            </form>
-        </div>
+            <div class="container">
+                <h1 id="register">Recuperar contraseña</h1>
+                <form id="registro"  onSubmit={handleSubmit(onSubmit)}>
+                    <label id="register" for="email">Introduce aquí tu correo electrónico para poder recuperar tu contraseña:</label>
+                    <input type="email" id="email" name="email" {...register('email', {
+                        required: true,
+                        pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+                    })} />
+                    {errors.email?.type === 'required' && <p>El campo correo es obligatorio</p>}
+                    {errors.email?.type === 'pattern' && <p>Ingresa un correo electrónico válido</p>}
+                    <button id="registrarse" type="submit">Recuperar contraseña</button>
+                </form>
+            </div>
         </>
     )
 }
